081d959f7bfebc241552315d83454f4fe5e5f3a7
Danny Cheng <a7932677@gmail.com>
2021-02-04 19:16:28 +0800

    + ProductController +routing resource with ProductController modify redirect path

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('products', ProductController::class);
+Route::get('/product/admin', [ProductController::class, 'admin']);
+
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
